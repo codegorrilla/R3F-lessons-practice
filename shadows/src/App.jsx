d9356bdc6e93b1dae33029ab665e37f3fd16c9c6
Sanjib Adhya <code.gorrilla@gmail.com>
2025-04-10 21:22:36 +0530

    fixed accumulated shadows

--- shadows/src/App.jsx
+++ shadows/src/App.jsx
@@ -1,34 +1,30 @@
-import { Canvas } from '@react-three/fiber';
 import {
 	AccumulativeShadows,
 	ContactShadows,
 	OrbitControls,
-	OrthographicCamera,
 	RandomizedLight,
-	SoftShadows,
-	useHelper,
 } from '@react-three/drei';
-import { useRef } from 'react';
+import { Canvas } from '@react-three/fiber';
 import { useControls } from 'leva';
-import * as THREE from 'three';
-
-export default function App() {
+function App() {
 	const { cubeInAir } = useControls({
 		cubeInAir: false,
 	});
+
 	return (
 		<>
 			<Canvas
-				camera={{ position: [0, 3, 3] }}
 				shadows
+				camera={{ position: [0, 3, 3] }}
 			>
+				<OrbitControls />
 				<AccumulativeShadows
 					temporal
 					frames={35}
 					alphaTest={0.85}
 					scale={5}
 					position={[0, -0.499, 0]}
-					color={'#efbd4e'}
+					color='#EFBD4E'
 				>
 					<RandomizedLight
 						amount={4}
@@ -39,94 +35,59 @@ export default function App() {
 					/>
 					<RandomizedLight
 						amount={4}
-						radius={9}
+						radius={5}
 						intensity={0.25}
 						ambient={0.55}
 						position={[-5, 5, -9]}
 					/>
 				</AccumulativeShadows>
-				{/* <SoftShadows /> */}
-				{/* <ContactShadows
+				<ContactShadows
 					position-y={-0.49}
 					opacity={0.5}
 					blur={2}
 					color={'pink'}
 					scale={10}
-				/> */}
-				<OrbitControls />
+				/>
 				<ambientLight intensity={0.5} />
 				<directionalLight
 					position={[5, 5, 5]}
 					intensity={0.5}
-					castShadow
-					//shadow-mapSize={[2048, 2048]}
-					// shadow-camera-far={20}
-					// shadow-camera-left={10}
-					// shadow-camera-right={-10}
-					// shadow-camera-top={-10}
-					// shadow-camera-bottom={10}
-				></directionalLight>
+				/>
 				<directionalLight
 					position={[-5, 5, 5]}
 					intensity={0.5}
-					color={'red'}
-					castShadow
+					color='red'
 				/>
 
 				<mesh
-					position={[1, 1.4, 1]}
+					position={[1, 1, 1]}
 					castShadow
 				>
 					<sphereGeometry args={[0.5, 32, 32]} />
-					<meshStandardMaterial color={'white'} />
-				</mesh>
-				<mesh
-					rotation-y={Math.PI / 4}
-					position-y={cubeInAir ? 1.5 : 1}
-					castShadow
-					receiveShadow
-				>
-					<boxGeometry />
-					<meshStandardMaterial color={'white'} />
+					<meshStandardMaterial color='white' />
 				</mesh>
 
-				{/* <mesh
+				<mesh
+					rotation-y={Math.PI / 4}
+					castShadow
+					receiveShadow
+					position-y={cubeInAir ? 1 : 0}
+				>
+					<boxGeometry />
+					<meshStandardMaterial color='white' />
+				</mesh>
+
+				<mesh
 					rotation-x={-Math.PI / 2}
-					position-y={0.5}
+					position-y={-0.5}
 					receiveShadow
 				>
 					<planeGeometry args={[5, 5]} />
-					<meshStandardMaterial color={'white'} />
-				</mesh> */}
-
-				{/* <group position={5}>
-					<mesh
-						position={[1, 2, 1]}
-						castShadow
-					>
-						<sphereGeometry args={[0.5, 32, 32]} />
-						<meshStandardMaterial color={'white'} />
-					</mesh>
-					<mesh
-						rotation-y={Math.PI / 4}
-						position-y={1}
-						castShadow
-						receiveShadow
-					>
-						<boxGeometry />
-						<meshStandardMaterial color={'white'} />
-					</mesh>
-
-					<mesh
-						rotation-x={-Math.PI / 2}
-						position-y={0.5}
-						receiveShadow
-					>
-						<planeGeometry args={[5, 5]} />
-						<meshStandardMaterial color={'white'} />
-					</mesh>
-				</group> */}
+					<meshStandardMaterial color='white' />
+				</mesh>
 			</Canvas>
 		</>
 	);
 }
+
+export default App;
